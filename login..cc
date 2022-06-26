fb17ac64bfbc9a14cf144be2b22ec8a30884f8af
byousaf <70032105+byousaf@users.noreply.github.com>
2022-06-26 21:39:05 +0100

    Create login..cc

---
+++ login..cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(void) {
+system ("clear");
+  string user;
+  string pass;
+  cout << "Username: "; 
+  cin >> user; 
+  system ("clear");
+  cout << "Password: ";
+  cin >> pass;
+  system ("clear");
+  cout << "Hello " << user << endl; 
+return 0;
+}
